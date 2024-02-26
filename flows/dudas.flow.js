7f1530f0555f84813d15fb96d1b5012b73c56bb0
JESUSLUG <jesus.lugo.conctacto@gmail.com>
2024-02-25 21:05:23 -0600

    BuenasNoches

--- flows/dudas.flow.js
+++ flows/dudas.flow.js
@@ -1,17 +1,8 @@
-const { addKeyword, EVENTS } = require('@bot-whatsapp/bot');
+const { addKeyword } = require('@bot-whatsapp/bot')
 
-module.exports = addKeyword('dudas', { caseSensitive: false }) 
-    .addAnswer('🤔 ¡Claro! Estoy aquí para ayudarte con cualquier duda que tengas.')
-    .addAnswer('Puedes preguntarme sobre nuestros productos, pedidos, horarios, etc.')
-    .addAnswer([
-        '¿Cuáles son sus productos más populares?',
-        '¿Cuál es el proceso para realizar un pedido?',
-        '¿Cuánto tiempo tardan en entregar los pedidos?',
-        
-    ], { capture: true }, null,
-    async (ctx, { captured }) => {
-       
-        const respuesta = `¡Entendido! Veo que tienes dudas sobre: ${captured}. Responderé tan pronto como pueda.`;
-        ctx.sendText(respuesta);
-    }
-);
+module.exports = addKeyword(['dudas', 'Dudas'])
+
+
+
+.addAnswer('👋 QUE DUDAS TIENES?',
+)
--- flows/welcome.flow.js
+++ flows/welcome.flow.js
@@ -1,18 +1,34 @@
-const { addKeyword, EVENTS } = require('@bot-whatsapp/bot')
-const cancelFlow = require('./cancel.flow')
-const rateFlow = require('./rate.flow')
-const commentsFlow = require('./comments.flow')
-const dudasFlow = require('./dudas.flow')
+const { addKeyword, EVENTS } = require('@bot-whatsapp/bot');
+const cancelFlow = require('./cancel.flow');
+const rateFlow = require('./rate.flow');
+const commentsFlow = require('./comments.flow');
+const dudasFlow = require('./dudas.flow');
+
+//  la hora del día
+function obtenerSaludo() {
+    const horaActual = new Date().getHours();
+
+    let saludo;
+    if (horaActual >= 5 && horaActual < 12) {
+        saludo = '¡Buen día!';
+    } else if (horaActual >= 12 && horaActual < 18) {
+        saludo = '¡Buenas tardes!';
+    } else {
+        saludo = '¡Buenas noches!';
+    }
+
+    return saludo + ' Bienvenido a la pastelería "La Condesa"';
+}
 
 module.exports = addKeyword(EVENTS.WELCOME)
-    .addAnswer('¡Buen día! Bienvenido a la pastelería \"La Condesa\"')
+    .addAnswer(obtenerSaludo())
     .addAnswer([
-        'Esribe la palabra del servicio te gustaría conocer:',
+        'Escribe la palabra del servicio que te gustaría conocer:',
         '👉 *ventas* para conocer nuestros productos',
-        '👉 *pedidos*  para ver el estado de tu pedido',
+        '👉 *pedidos* para ver el estado de tu pedido',
         '👉 *cancelar* para terminar la conversación en cualquier momento',
-        '👉 *dudas* Para saber quienes son y tengo dudas'
+        '👉 *dudas* para saber quiénes somos y tengo dudas'
     ], 
     { capture: true, delay: 3000 }, null,
-    [rateFlow, commentsFlow, cancelFlow]
-)
+    [rateFlow, commentsFlow, cancelFlow, dudasFlow]
+);
